a7f2273c77b0446518d4a87b985f34fc2ae8a953
Alisher Kenzhebek uulu <alisher.andakeev@gmail.com>
2024-02-15 13:32:16 +0600

    commit

--- computers/urls.py
+++ computers/urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('accessories-list/', views.AccessoriesList.as_view()),
-    # path('accessories-create/', views.AccesoriesCreate.as_view()),
-    # path('accessories-<int:pk>/', views.AccessoriesReUpDelete.as_view()),
 
     path('brands-list/', views.BrandsList.as_view()),
     path('brands-create/', views.BrandCreate.as_view()),
